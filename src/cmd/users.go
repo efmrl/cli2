e6da46493439980f50ed0a90539e9e02190b24f8
Sam Falkner <sam.falkner@gmail.com>
2024-04-14 14:23:46 -0600

    you can update a user

--- src/cmd/users.go
+++ src/cmd/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"path"
@@ -11,10 +12,13 @@ import (
 )
 
 type UserCmd struct {
-	Get GetUser `cmd:"" help:"get current user"`
+	Get    GetUser    `cmd:"" help:"get current user"`
+	Update UpdateUser `cmd:"" help:"update a user"`
 }
 
 type GetUser struct {
+	UserID string `opt:"" help:"user ID; default current user"`
+
 	ts *httptest.Server
 }
 
@@ -25,7 +29,16 @@ func (gu *GetUser) Run(ctx *CLIContext) error {
 	}
 	cfg.ts = gu.ts
 
-	url, err := getUserPath(cfg)
+	userID := gu.UserID
+	if userID == "" {
+		userID, err = getCurrentUserID(cfg)
+		if err != nil {
+			err = fmt.Errorf("cannot get current user ID: %w", err)
+			return err
+		}
+	}
+
+	url := getUserPath(cfg, userID)
 	if err != nil {
 		return err
 	}
@@ -53,11 +66,60 @@ func (gu *GetUser) Run(ctx *CLIContext) error {
 	return nil
 }
 
-func getUserPath(cfg *Config) (*url.URL, error) {
+type UpdateUser struct {
+	UserID string `opt:"" help:"user ID; default current user"`
+	Name   string `opt:"" help:"the name for the user"`
+
+	ts *httptest.Server
+}
+
+func (uu *UpdateUser) Run(ctx *CLIContext) error {
+	cfg, err := loadConfig()
+	if err != nil {
+		return err
+	}
+	cfg.ts = uu.ts
+
+	userID := uu.UserID
+	if userID == "" {
+		userID, err = getCurrentUserID(cfg)
+		if err != nil {
+			err = fmt.Errorf("cannot get current user: %w", err)
+			return err
+		}
+	}
+	url := getUserPath(cfg, userID)
+	client, err := cfg.getClient()
+	if err != nil {
+		return err
+	}
+
+	req := &api2.NewUser{
+		Name: uu.Name,
+	}
+	resp, err := patchJSON(ctx.Context, client, url, req, nil)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		err = fmt.Errorf("error on update: %v", resp.Status)
+		return err
+	}
+
+	return nil
+}
+
+func getUserPath(cfg *Config, userID string) *url.URL {
+	path := path.Join("users", userID, "data")
+	url := cfg.pathToAPIurl(path)
+	return url
+}
+
+func getCurrentUserID(cfg *Config) (string, error) {
 	url := cfg.pathToAPIurl("session")
 	client, err := cfg.getClient()
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	res := &api2.NewSessionRes{}
@@ -65,15 +127,12 @@ func getUserPath(cfg *Config) (*url.URL, error) {
 	err = httpGetJSON(client, url, nsRes)
 	if err != nil {
 		err = fmt.Errorf("cannot get session: %w", err)
-		return nil, err
+		return "", err
 	}
 
 	if res.UserID == "" {
-		return nil, fmt.Errorf("not logged in")
+		return "", fmt.Errorf("not logged in")
 	}
 
-	path := path.Join("users", res.UserID, "data")
-	url = cfg.pathToAPIurl(path)
-
-	return url, nil
+	return res.UserID, nil
 }
